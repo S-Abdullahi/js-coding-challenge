cf7354b5151d49e16a7a2c3d27c78605532345df
Abdullahi Salawu <salabdullahi33@gmail.com>
2022-12-13 21:26:58 +0100

    string practice

--- section9/script.js
+++ section9/script.js
@@ -343,3 +343,16 @@ function convertString(text){
 }
 
 convertString('salawu_abdullahi')
+
+
+// Data needed for a later exercise
+const flights =
+  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+
+const flight_split = flights.split('+')
+for(const i of flight_split){
+    const [type, from, to, time] = i.split(';')
+    const output = `${type.startsWith('_Delayed') ? '🎈' : ''} ${type.replaceAll('_',' ')} from ${from.slice(0,3).toUpperCase()} to ${to.slice(0,3).toUpperCase()} (${time.replace(':','h')})`
+    console.log(output)
+}
+
