74298c29f4dfb2ca512a30c37c4ecb9dc9f2a946
Abdullahi Salawu <salabdullahi33@gmail.com>
2022-12-14 07:47:26 +0100

    call and apply method

--- section10/script.js
+++ section10/script.js
@@ -27,3 +27,45 @@ const transformer = function(str, fn){
 }
 
 transformer('Abdullahi is my name', uppperFirstWord)
+
+const greet = function(greeting){
+    return function(name){
+        console.log(`${greeting} ${name}`)
+    }
+}
+const greetArrow = greeting => name => console.log(`${greeting} ${name}`)
+const prac = (food)=>{
+    return (items)=>{
+        console.log(`${food} ${items}`)
+    }
+}
+// const greeter = greet('hello')
+greet('as-salam aleikum')('Abdullahi')
+prac('yam')('egg')
+greetArrow('you!')('Olalekan')
+
+const luthanza = {
+    airline: 'Luthanza',
+    iataCode: 'LH',
+    bookings: [],
+    book(flightNum, name){
+        console.log(`${name} booked a seat on ${this.airline} flight ${this.iataCode}${flightNum}`)
+        this.bookings.push(
+            {
+                flight: `${this.iataCode}${flightNum}`, name
+            }
+        )
+    }
+}
+
+
+const swiss = {
+    airline: 'Swiss',
+    iataCode: 'SW',
+    bookings: [],
+}
+luthanza.book(245, 'Salawu Abdullahi')
+console.log(luthanza)
+
+const book = luthanza.book
+book.call(swiss, 444, 'Bankole Hameedah')
