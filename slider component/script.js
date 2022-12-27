f07a36df74be0472364139b820f162b2a03e2bbc
Abdullahi Salawu <salabdullahi33@gmail.com>
2022-12-27 10:14:45 +0100

    slide functionalities

--- slider component/script.js
+++ slider component/script.js
@@ -1,8 +1,27 @@
 const btnRight = document.querySelector('.btn-right')
 const btnLeft = document.querySelector('.btn-left')
-
 const slide = document.querySelectorAll('.image')
+const dotContainer = document.querySelector('.dots-con')
 
+//create dot for slide
+function createDot(){
+    slide.forEach((_,i)=>{
+        document.querySelector('.dots-con').insertAdjacentHTML('beforeend',`<div class="dot" data-slide="${i}"></div>`)
+    })
+}
+
+//activate dots during slide
+function activateDot(slideNum){
+    //remove active class from all the dots
+    document.querySelectorAll('.dot').forEach((dot)=>{
+        dot.classList.remove('dot-active')
+    })
+
+    //add active class to specific dot
+    document.querySelector(`.dot[data-slide="${slideNum}"]`).classList.add('dot-active')
+}
+
+//change slide
 let currentSlide = 0
 const maxSlide = slide.length
 
@@ -12,8 +31,16 @@ function slideSide(curr){
     })
 }
 
-slideSide(0)
+//initial function
+function init(){
+    createDot()
+    activateDot(0)
+    slideSide(0)
+}
+init()
 
+
+//next slide functionality
 function nextSlide(){
     if(currentSlide === maxSlide-1){
         currentSlide = 0
@@ -21,8 +48,10 @@ function nextSlide(){
         currentSlide++
     }
     slideSide(currentSlide)
+    activateDot(currentSlide)
 }
 
+//previous slide functionality
 function previousSlide(){
     if(currentSlide === 0){
         currentSlide = maxSlide -1
@@ -30,7 +59,23 @@ function previousSlide(){
         currentSlide--
     }
     slideSide(currentSlide)
+    activateDot(currentSlide)
 }
 
+//event listeners
 btnRight.addEventListener('click', nextSlide)
 btnLeft.addEventListener('click', previousSlide)
+
+dotContainer.addEventListener('click',(e)=>{
+    if(e.target.classList.contains('dot')){
+        const currDot = e.target.dataset.slide
+        slideSide(currDot)
+        activateDot(currDot)
+    }
+})
+
+//keyboard arrow to slide
+document.addEventListener('keydown',(e)=>{
+    e.key=='ArrowRight' && nextSlide()
+    e.key == 'ArrowLeft' && previousSlide()
+})
